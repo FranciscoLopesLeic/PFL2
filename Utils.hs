1bcfc72c373f794bf12e16b39ba5d5ee574abec9
Francisco Lopes <up202108796@up.pt>
2023-12-30 12:28:53 +0000

    utils fixed

--- Utils.hs
+++ Utils.hs
@@ -21,35 +21,35 @@ recursaostack2Str (Booleano True:rest) acc = recursaostack2Str rest (acc ++ "Tru
 recursaostack2Str (Booleano False:rest) acc = recursaostack2Str rest (acc ++ "False,")
 recursaostack2Str (Inteiro s:rest) acc = recursaostack2Str rest (acc ++ show s ++ ",")
 
-findState :: State -> String -> Maybe StackItem
+finding :: State -> String -> Maybe StackItem
 
-findState [] _ = Nothing
-findState ((str,b):code) c
+finding [] _ = Nothing
+finding ((str,b):code) c
                           | str == c = Just b
-                          | otherwise = findState code c
+                          | otherwise = finding code c
 
-sortState :: State -> State
-sortState = sortBy (comparing fst)
+sorting :: State -> State
+sorting = sortBy (comparing fst)
 
-getJustInt :: Maybe StackItem -> StackItem
-getJustInt (Just (Inteiro a)) = Inteiro a
-getJustInt (Just (Booleano a)) = Booleano a
-getJustInt _ = error "Run-time error"
+getInteiros :: Maybe StackItem -> StackItem
+getInteiros (Just (Inteiro a)) = Inteiro a
+getInteiros (Just (Booleano a)) = Booleano a
+getInteiros _ = error "Run-time error"
 
 state2Str :: State -> String
 state2Str [] = ""
-state2Str state = init (recursaostate2Str (sortState state) "")
+state2Str state = init (recursaostate2Str (sorting state) "")
 
 recursaostate2Str :: State -> String -> String
 recursaostate2Str [] acc = acc
 recursaostate2Str ((str, Booleano a):code) acc = recursaostate2Str code (acc ++ str ++ "=" ++ show a ++ ",")
 recursaostate2Str ((str, Inteiro a):code) acc = recursaostate2Str code (acc ++ str ++ "=" ++ show a ++ ",")
 
-subsState :: State -> String -> StackItem -> State
-subsState state str int = recursaosubsState state str int []
+repState :: State -> String -> StackItem -> State
+repState state str int = recursaorepState state str int []
 
-recursaosubsState :: State -> String -> StackItem -> State -> State
-recursaosubsState [] str b acc = reverse ((str,b):acc)
-recursaosubsState ((notstr,b):code) str c acc
+recursaorepState :: State -> String -> StackItem -> State -> State
+recursaorepState [] str b acc = reverse ((str,b):acc)
+recursaorepState ((notstr,b):code) str c acc
                                       | notstr == str = reverse acc ++ ((str, c) : code)
-                                      | otherwise = recursaosubsState code str c ((notstr,b):acc)
+                                      | otherwise = recursaorepState code str c ((notstr,b):acc)
